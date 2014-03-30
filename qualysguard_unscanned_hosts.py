9e4890ab1a4cbf14b570e021a8e0920a49f8630a
Parag Baxi <ghee22@gmail.com>
2014-03-30 09:54:43 -0400

    print to file

--- qualysguard_unscanned_hosts.py
+++ qualysguard_unscanned_hosts.py
@@ -1,7 +1,8 @@
+from __future__ import print_function
 import argparse
 import datetime
 from collections import defaultdict
-import logging
+import logging, sys
 import netaddr
 import os
 import qualysapi
@@ -13,10 +14,11 @@ def download_paginated(call, param, tag):
     global qgc
     # Start with first host, which starts at 1.
     param['id_min'] = 1
-    ips = set()
-    # Start downloading of all.
+    ips = netaddr.IPSet()
+    # Start downloading all.
+    id_min = 1
     while True:
-        print 'Downloading hosts from ID %d' % id_min
+        print('Downloading hosts from ID %d' % id_min)
         xml = qgc.request(call, param)
         # Parse.
         tree = etree.fromstring(xml)
@@ -36,19 +38,22 @@ def download_paginated(call, param, tag):
     return ips
 
 def ips_in_ip_list(xml):
-    """Extract IPs from IP list.
+    """Return set of extracted IPs from IP list XML.
     """
     tree = etree.fromstring(xml)
-    ip_range = []
+    ips = netaddr.IPSet()
     # Grab all IPs and IP ranges.
     ip_list = tree.xpath('//IP_SET/descendant::*/text()')
     for i in ip_list:
+        print i
         if '-' in i:
             ip_start = i[:i.find('-')]
             ip_end = i[i.find('-')+1:]
-            ip_range += list(netaddr.iter_iprange(ip_start, ip_end))
-    else:
-        ip_range.append(i)
+            # ip_range += list(netaddr.iter_iprange(ip_start, ip_end))
+            ips.add(netaddr.IPRange(ip_start,ip_end))
+        else:
+            ips.add(i)
+    return ips
 
 #
 #  Begin
@@ -58,7 +63,7 @@ parser = argparse.ArgumentParser(description = 'What QualysGuard hosts have I no
 parser.add_argument('-t', '--truncation_limit',
                     default=1000,
                     help = 'How many hosts to download per call. (Default = 1000')
-parser.add_argument('-v', '--debug',
+parser.add_argument('-v', '--verbose',
                     action = 'store_true',
                     help = 'Outputs additional information to log.')
 parser.add_argument('--config',
@@ -96,14 +101,25 @@ else:
 subscribed_ips = download_paginated('/api/2.0/fo/asset/host/',
                                     {'action': 'list', 'truncation_limit': c_args.truncation_limit},
                                     'HOST')
+logger.debug('subscribed_ips = \n%s' % str(subscribed_ips))
 # Find VM subscribed IPs.
-vm_ips_xml = qgc.request('/api/2.0/fo/asset/host/',
-    {'action': 'list', 'compliance_enabled', '0'})
-vm_ips = ips_in_ip_list(vm_ips_xml)
-tree.xpath('//IP_SET/descendant::*/text()')
+scanned_vm_ips_xml = qgc.request('/api/2.0/fo/asset/host/',
+                         {'action': 'list', 'compliance_enabled': '0'})
+scanned_vm_ips = ips_in_ip_list(scanned_vm_ips_xml)
+logger.debug('scanned_vm_ips = \n%s' % str(scanned_vm_ips))
+# Print to file VM IPs not scanned but subscribed.
+vm_unscanned = subscribed_ips.remove(scanned_vm_ips)
+with open('vm_unscanned.txt', 'w') as f:
+    for ip in vm_unscanned:
+        print(ip, file=f)
 # Find PC subscribed IPs.
-pc_ips_xml = qgc.request('/api/2.0/fo/asset/host/',
-    {'action': 'list', 'compliance_enabled', '1'})
-pc_ips = ips_in_ip_list(pc_ips_xml)
-
+scanned_pc_ips_xml = qgc.request('/api/2.0/fo/asset/host/',
+                         {'action': 'list', 'compliance_enabled': '1'})
+scanned_pc_ips = ips_in_ip_list(scanned_pc_ips_xml)
+logger.debug('scanned_pc_ips = \n%s' % str(scanned_pc_ips))
+# Print to file PC IPs not scanned but subscribed.
+pc_unscanned = subscribed_ips.remove(scanned_pc_ips)
+with open('pc_unscanned.txt', 'w') as f:
+    for ip in pc_unscanned:
+        print(ip, file=f)
 exit()
